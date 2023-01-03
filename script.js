c4decf8cc0ddde35da48f97d644af8b522a871e0
Julie Gejel <juli71n7@stud.kea.dk>
2023-01-03 15:08:12 +0100

    forside animation

---
+++ script.js
@@ -0,0 +1,10 @@
+window.addEventListener("load", skillBar);
+
+function skillBar() {
+  console.log("skillbarstart");
+  document.querySelector("#skillbar_sprite").classList.add("tid");
+  document.querySelector("#skillbar_sprite1").classList.add("tid1");
+  document.querySelector("#skillbar_sprite2").classList.add("tid2");
+  document.querySelector("#skillbar_sprite3").classList.add("tid3");
+  document.querySelector("#skillbar_sprite4").classList.add("tid4");
+}
