51bf281318579d77c2dc380be7375457a883394d
Julie Gejel <juli71n7@stud.kea.dk>
2023-01-10 14:34:26 +0100

    flere rettelser

--- burger.js
+++ burger.js
@@ -1,14 +1,19 @@
 const burger = document.querySelector(".burger");
 const navMenu = document.querySelector(".nav-menu");
 
-burger.addEventListener("click", () => {
-  burger.classList.toggle("active");
-  navMenu.classList.toggle("active");
-});
+window.addEventListener("load", sidenVises);
+function sidenVises() {
+  console.log("sidenvises");
 
-document.querySelectorAll(".nav-link").forEach((n) =>
-  n.addEventListener("click", () => {
-    burger.classList.remove("active");
-    navMenu.classList.remove("active");
-  })
-);
+  burger.addEventListener("click", () => {
+    burger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+  });
+
+  document.querySelectorAll(".nav-link").forEach((n) =>
+    n.addEventListener("click", () => {
+      burger.classList.remove("active");
+      navMenu.classList.remove("active");
+    })
+  );
+}
